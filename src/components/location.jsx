8513b659e993bdd50e5383935dc42a7596f1ac14
ChangHyunChoe <chchoe@com2us.com>
2025-01-22 09:12:18 +0900

    위치 설정 정보 업데이트

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -47,10 +47,10 @@ const Location = () => {
   const executeScript = () => {
     const scriptTag = document.createElement("script");
     const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
-      "timestamp" : "1737463210340",
-      "key" : "2muf4",
-      "mapWidth" : "640",
-      "mapHeight" : "360"
+    "timestamp" : "1737504507025",
+    "key" : "2mugf",
+    "mapWidth" : "640",
+    "mapHeight" : "360"
   }).render();`);
     scriptTag.appendChild(inlineScript);
     document.body.appendChild(scriptTag);
